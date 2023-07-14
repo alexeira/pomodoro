dff9f4353f2a23e957f779e313142e414911cc8e
alexeira <alex.pincheira06@gmail.com>
2023-07-14 02:26:59 -0400

    add icons components to src

---
+++ src/components/icons/Play.tsx
@@ -0,0 +1,12 @@
+export function Play() {
+  return (
+    <svg fill="none" height={50} width={49} xmlns="http://www.w3.org/2000/svg">
+      <ellipse cx={24.621} cy={25} fill="#572D1C" rx={24.379} ry={25} />
+      <path
+        className="play"
+        d="M35.615 22.849c1.635.968 1.635 3.334 0 4.302l-14.534 8.605c-1.666.987-3.774-.214-3.774-2.151v-17.21c0-1.937 2.108-3.138 3.774-2.151l14.534 8.605Z"
+        fill="#FFF5EE"
+      />
+    </svg>
+  )
+}
