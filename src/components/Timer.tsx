ac91fe276070379240a2df21328594b61bfd2bcf
alexeira <alex.pincheira06@gmail.com>
2023-07-14 02:24:39 -0400

    remove unused console.log and add className for timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -15,19 +15,14 @@ const Timer = () => {
 
   const formatTime = (time: number) => {
     const minutes = Math.floor(time / 60)
-
-    console.log(minutes, 'minutes')
     const seconds = time % 60
-
-    console.log(seconds, 'seconds')
 
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
   }
 
   return (
     <div>
-      <h1>Temporizador de 25 minutos</h1>
-      <div>{formatTime(timeRemaining)}</div>
+      <h1 className="timer">{formatTime(timeRemaining)}</h1>
     </div>
   )
 }
