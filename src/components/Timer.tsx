6d0535ccd769ba6072e6c28e828bf8de91b6639c
alexeira <alex.pincheira06@gmail.com>
2023-07-15 20:26:35 -0400

    use custom hook for state instead of useState

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,30 +1,36 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 
-const Timer = () => {
-  const [timeRemaining, setTimeRemaining] = useState(1500)
+import { usePomodoro } from '../hooks/usePomodoro'
+
+export default function Timer() {
+  const { pomodoro, setPomodoro } = usePomodoro()
+  const { timeRemaining } = pomodoro
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setTimeRemaining(prevTime => prevTime - 1)
+      setPomodoro(prevPomodoro => ({
+        ...prevPomodoro,
+        timeRemaining: prevPomodoro.timeRemaining - 1
+      }))
     }, 1000)
 
     return () => {
       clearInterval(timer)
     }
-  }, [])
+  }, [setPomodoro])
 
-  const formatTime = (time: number) => {
+  function formatTime(time: number) {
     const minutes = Math.floor(time / 60)
     const seconds = time % 60
 
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
   }
 
+  const formatedTime = formatTime(timeRemaining)
+
   return (
     <div>
-      <h1 className="timer">{formatTime(timeRemaining)}</h1>
+      <h1 className="timer">{formatedTime}</h1>
     </div>
   )
 }
-
-export default Timer
