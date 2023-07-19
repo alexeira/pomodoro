64d84deb2d3df5bb0df8b404e2360919e98203c8
alexeira <alex.pincheira06@gmail.com>
2023-07-19 02:01:19 -0400

    create Button.tsx for reuse buttons components

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,36 @@
+import { Next } from './icons/Next'
+import { Pause } from './icons/Pause'
+import { Play } from './icons/Play'
+import { Stop } from './icons/Stop'
+
+export function StopButton({ stopTimer }: { stopTimer: () => void }) {
+  return (
+    <button data-title="Parar" onClick={stopTimer}>
+      <Stop />
+    </button>
+  )
+}
+
+export function PlayButton({ startTimer }: { startTimer: () => void }) {
+  return (
+    <button data-title="Reproducir" onClick={startTimer}>
+      <Play />
+    </button>
+  )
+}
+
+export function PauseButton({ pauseTimer }: { pauseTimer: () => void }) {
+  return (
+    <button data-title="Pausar" onClick={pauseTimer}>
+      <Pause />
+    </button>
+  )
+}
+
+export function NextButton() {
+  return (
+    <button data-title="Siguiente">
+      <Next />
+    </button>
+  )
+}
