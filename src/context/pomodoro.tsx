78d4eeb81012e8690926e132692105ae0ea3d78f
alexeira <alex.pincheira06@gmail.com>
2023-07-19 02:10:28 -0400

    feat(pomodoro): provide the interval logic on context

--- src/context/pomodoro.tsx
+++ src/context/pomodoro.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 
 import { Pomodoro, PomodoroContextProps } from '../interfaces'
 
@@ -10,8 +10,60 @@ export function PomodoroProvider({ children }: { children: React.ReactNode }) {
     doing: 'studying'
   })
 
+  const [isRunning, setIsRunning] = useState(false)
+
+  function handleInterval() {
+    setPomodoro(prevPomodoro => ({
+      ...prevPomodoro,
+      timeRemaining: prevPomodoro.timeRemaining - 1
+    }))
+  }
+
+  useEffect(() => {
+    const { timeRemaining } = pomodoro
+    let timer: NodeJS.Timeout | number = 0
+
+    if (isRunning && timeRemaining > 0) {
+      timer = setInterval(() => {
+        handleInterval()
+      }, 1000)
+    }
+
+    if (!isRunning || timeRemaining === 0) {
+      clearInterval(timer)
+    }
+
+    return () => clearInterval(timer)
+  }, [isRunning, pomodoro])
+
+  function startTimer() {
+    setIsRunning(true)
+    handleInterval()
+  }
+
+  function pauseTimer() {
+    setIsRunning(false)
+  }
+
+  function stopTimer() {
+    setIsRunning(false)
+    setPomodoro({
+      timeRemaining: 1500,
+      doing: 'studying'
+    })
+  }
+
   return (
-    <PomodoroContext.Provider value={{ pomodoro, setPomodoro }}>
+    <PomodoroContext.Provider
+      value={{
+        pomodoro,
+        isRunning,
+        setPomodoro,
+        startTimer,
+        pauseTimer,
+        stopTimer
+      }}
+    >
       {children}
     </PomodoroContext.Provider>
   )
