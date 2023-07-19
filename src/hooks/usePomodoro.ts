5d1b2f4ac91317cd3c6e441b27354e6c64825a44
alexeira <alex.pincheira06@gmail.com>
2023-07-19 02:09:42 -0400

    feat(timer): add formatTime function to custom hook

--- src/hooks/usePomodoro.ts
+++ src/hooks/usePomodoro.ts
@@ -5,9 +5,17 @@ import { PomodoroContextProps } from '../interfaces'
 
 export function usePomodoro() {
   const pomodoroContext = useContext(PomodoroContext)
-  const { pomodoro, setPomodoro } = pomodoroContext as PomodoroContextProps
+  const { pomodoro, isRunning, setPomodoro, startTimer, pauseTimer, stopTimer } =
+    pomodoroContext as PomodoroContextProps
 
-  function handleTime() {}
+  function formatTime(time: number) {
+    const minutes = Math.floor(time / 60)
+    const seconds = time % 60
 
-  return { pomodoro, setPomodoro }
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+  }
+
+  const formatedTime = formatTime(pomodoro.timeRemaining)
+
+  return { pomodoro, isRunning, formatedTime, setPomodoro, startTimer, pauseTimer, stopTimer }
 }
