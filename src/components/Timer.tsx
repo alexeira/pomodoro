7ffcc37c3693db037764d502abfb8a621b9f69b2
alexeira <alex.pincheira06@gmail.com>
2023-07-19 02:10:59 -0400

    clean component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,32 +1,7 @@
-import { useEffect } from 'react'
-
 import { usePomodoro } from '../hooks/usePomodoro'
 
 export default function Timer() {
-  const { pomodoro, setPomodoro } = usePomodoro()
-  const { timeRemaining } = pomodoro
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setPomodoro(prevPomodoro => ({
-        ...prevPomodoro,
-        timeRemaining: prevPomodoro.timeRemaining - 1
-      }))
-    }, 1000)
-
-    return () => {
-      clearInterval(timer)
-    }
-  }, [setPomodoro])
-
-  function formatTime(time: number) {
-    const minutes = Math.floor(time / 60)
-    const seconds = time % 60
-
-    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
-  }
-
-  const formatedTime = formatTime(timeRemaining)
+  const { formatedTime } = usePomodoro()
 
   return (
     <div>
