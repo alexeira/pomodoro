f1d729c781ffd565f535428ed267655d385f1294
alexeira <alex.pincheira06@gmail.com>
2023-07-18 23:00:07 -0400

    provide the pomodoro context

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App'
 import './styles.css'
+import { PomodoroProvider } from './context/pomodoro'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <PomodoroProvider>
+    <App />
+  </PomodoroProvider>
+)
