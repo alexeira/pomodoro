db9e6bb9dfc6fbe0b34402076b351f76f4cca721
alexeira <alex.pincheira06@gmail.com>
2023-07-20 15:15:06 -0400

    fix(import): add new folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Container } from './components/layout/Container'
 import { TitleBar } from './components/layout/TitleBar'
-import { Pomodoro } from './components/Pomodoro'
-import { TimerMenu } from './components/TimerMenu'
+import { Pomodoro } from './components/pomodoro/Pomodoro'
+import { TimerMenu } from './components/menu/TimerMenu'
 
 export default function App() {
   return (
