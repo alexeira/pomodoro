48b658cdb5aaeadef9d1d521b962bc5bed0506ba
alexeira <alex.pincheira06@gmail.com>
2023-07-19 22:26:27 -0400

    feat(pomodoro): add component CycleCounter to Doing layout

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -1,7 +1,13 @@
+import { CycleCounter } from './CycleCounter'
 import Timer from './Timer'
 
 function Doing({ task }: { task: string }) {
-  return <span className="doing">{task}</span>
+  return (
+    <div>
+      <span className="doing">{task}</span>
+      <CycleCounter />
+    </div>
+  )
 }
 
 export function Pomodoro() {
