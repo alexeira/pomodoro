d1956c957b177e0fc03b684b5ff703450aa232b1
alexeira <alex.pincheira06@gmail.com>
2023-07-21 22:39:02 -0400

    fix(svg): correct use of svgs for buttons

--- src/components/icons/TimerMenuIo.tsx
+++ src/components/icons/TimerMenuIo.tsx
@@ -1,19 +1,12 @@
 export function Stop() {
-  return (
-    <svg fill="none" height={50} width={50} xmlns="http://www.w3.org/2000/svg">
-      <circle cx={25} cy={25} fill="#572D1C" r={25} />
-      <rect fill="#FFF5EE" height={22} rx={4} width={22} x={14} y={14} />
-    </svg>
-  )
+  return <div style={{ minWidth: 22, minHeight: 22, borderRadius: 4, background: '#fff5ee' }} />
 }
 
 export function Play() {
   return (
-    <svg fill="none" height={50} width={49} xmlns="http://www.w3.org/2000/svg">
-      <ellipse cx={24.621} cy={25} fill="#572D1C" rx={24.379} ry={25} />
+    <svg fill="none" height={24} width={21} xmlns="http://www.w3.org/2000/svg">
       <path
-        className="play"
-        d="M35.615 22.849c1.635.968 1.635 3.334 0 4.302l-14.534 8.605c-1.666.987-3.774-.214-3.774-2.151v-17.21c0-1.937 2.108-3.138 3.774-2.151l14.534 8.605Z"
+        d="M19.288 9.45c2.026 1.096 2.026 4.004 0 5.1L4.28 22.673C2.348 23.72 0 22.32 0 20.123V3.877C0 1.68 2.348.28 4.28 1.327L19.288 9.45Z"
         fill="#FFF5EE"
       />
     </svg>
@@ -22,9 +15,8 @@ export function Play() {
 
 export function Pause() {
   return (
-    <svg fill="none" height={50} width={49} xmlns="http://www.w3.org/2000/svg">
-      <ellipse cx={24.379} cy={25} fill="#572D1C" rx={24.379} ry={25} />
-      <path d="M16 15v19M32 15v19" stroke="#FFF5EE" strokeLinecap="round" strokeWidth={8} />
+    <svg fill="none" height={22} width={20} xmlns="http://www.w3.org/2000/svg">
+      <path d="M17 3v16M3 3v16" stroke="#FFF5EE" strokeLinecap="round" strokeWidth={6} />
     </svg>
   )
 }
@@ -33,11 +25,10 @@ export function Next() {
   return (
     <svg fill="none" height={26} width={22} xmlns="http://www.w3.org/2000/svg">
       <path
-        className="next"
         d="M18.308 10.858c1.635.968 1.635 3.335 0 4.303L3.774 23.766C2.107 24.752 0 23.55 0 21.615V4.404c0-1.937 2.107-3.138 3.774-2.152l14.534 8.605Z"
         fill="#773f29"
       />
-      <path className="next" d="M20 4v18" stroke="#773f29" strokeLinecap="round" strokeWidth={4} />
+      <path d="M20 4v18" stroke="#773f29" strokeLinecap="round" strokeWidth={4} />
     </svg>
   )
 }
