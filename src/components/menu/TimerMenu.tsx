f55a5d4fc87d8af41ac2d04edd6520dd1555faa7
alexeira <alex.pincheira06@gmail.com>
2023-07-22 22:57:10 -0400

    styles(timer-menu): add animations for timer menu

--- src/components/menu/TimerMenu.tsx
+++ src/components/menu/TimerMenu.tsx
@@ -6,7 +6,7 @@ export function TimerMenu() {
   const { isRunning, startTimer, pauseTimer, stopTimer, nextCycle } = usePomodoro()
 
   return (
-    <section className="buttons-section">
+    <section className={`buttons-section ${isRunning ? 'hidden-menu' : ''}`}>
       <StopButton isRunning={isRunning} stopTimer={stopTimer} />
       {isRunning ? <PauseButton pauseTimer={pauseTimer} /> : <PlayButton startTimer={startTimer} />}
       <NextButton isRunning={isRunning} nextCycle={nextCycle} />
