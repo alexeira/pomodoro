c9d5d01204afeb9d448ddc812e6b7488a09a77b7
alexeira <alex.pincheira06@gmail.com>
2023-07-22 22:55:30 -0400

    feat(keydown): add event listeners for keyboard use

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
+import { useEffect } from 'react'
+
 import { Container } from './components/layout/Container'
 import { TitleBar } from './components/layout/TitleBar'
 import { Pomodoro } from './components/pomodoro/Pomodoro'
 import { TimerMenu } from './components/menu/TimerMenu'
+import { usePomodoro } from './hooks/usePomodoro'
 
 export default function App() {
+  const { isRunning, startTimer, pauseTimer, stopTimer, nextCycle } = usePomodoro()
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === ' ') {
+        event.preventDefault()
+        isRunning ? pauseTimer() : startTimer()
+      }
+      if (event.key === 'Backspace') {
+        event.preventDefault()
+        stopTimer()
+      }
+      if (event.key === 'ArrowRight') {
+        event.preventDefault()
+        nextCycle()
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [isRunning, startTimer, pauseTimer, stopTimer, nextCycle])
+
   return (
     <>
-      <TitleBar />
       <Container>
+        <TitleBar />
         <TimerMenu />
         <Pomodoro />
       </Container>
