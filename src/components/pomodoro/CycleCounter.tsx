61229e9c330ef75608a89a1c9080b55dd2d86b9a
alexeira <alex.pincheira06@gmail.com>
2023-07-22 23:00:02 -0400

    styles(cycle-counter): add animations for cycle counter

--- src/components/pomodoro/CycleCounter.tsx
+++ src/components/pomodoro/CycleCounter.tsx
@@ -1,8 +1,20 @@
+import { useRef } from 'react'
+
 import { usePomodoro } from '../../hooks/usePomodoro'
 
 export function CycleCounter() {
-  const { pomodoro } = usePomodoro()
+  const { pomodoro, isRunning } = usePomodoro()
   const { cycleCounter } = pomodoro
+  const prevCycleCounter = useRef(cycleCounter)
 
-  return <span className="cycle-counter">#{cycleCounter}</span>
+  return (
+    <span
+      key={cycleCounter}
+      className={`text-doing cycle-counter ${
+        !isRunning && cycleCounter > prevCycleCounter.current ? 'cycle-counter-animation' : ''
+      }`}
+    >
+      #{cycleCounter}
+    </span>
+  )
 }
