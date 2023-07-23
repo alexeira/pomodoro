f7748398e8fd2aa10171346c093ec689b163be24
alexeira <alex.pincheira06@gmail.com>
2023-07-22 23:00:43 -0400

    feat(title-bar): add native button behavior

--- src/components/layout/TitleBar.tsx
+++ src/components/layout/TitleBar.tsx
@@ -1,16 +1,32 @@
-import { Config, Exit, Min } from '../icons/TitleBarIo'
+import { appWindow } from '@tauri-apps/api/window'
+
+import { Exit, Min } from '../icons/TitleBarIo'
+
 import './TitleBar.css'
 
 export function TitleBar() {
+  async function closeWindow() {
+    await appWindow.close()
+  }
+
+  async function minimizeWindow() {
+    await appWindow.minimize()
+  }
+
   return (
-    <div data-tauri-drag-region className="titlebar">
-      <button className="titlebar-button" id="titlebar-config">
+    <div data-tauri-drag-region className="titlebar" tabIndex={-1}>
+      {/* <button className="titlebar-button" id="titlebar-config">
         <Config />
-      </button>
-      <button className="titlebar-button" id="titlebar-minimize">
+      </button> */}
+      <button
+        className="titlebar-button"
+        id="titlebar-minimize"
+        tabIndex={-1}
+        onClick={minimizeWindow}
+      >
         <Min />
       </button>
-      <button className="titlebar-button" id="titlebar-close">
+      <button className="titlebar-button" id="titlebar-close" tabIndex={-1} onClick={closeWindow}>
         <Exit />
       </button>
     </div>
