bba4ac02c4c69a7010a99c73c4bcd0edd3cbf3d3
alexeira <alex.pincheira06@gmail.com>
2023-07-22 23:01:17 -0400

    fix(pomodoro): adjust component and fix styles

--- src/components/pomodoro/Pomodoro.tsx
+++ src/components/pomodoro/Pomodoro.tsx
@@ -1,13 +1,15 @@
-import Timer from './Timer'
+import { Cycle } from './Cycle'
 import { Doing } from './Doing'
+import { Timer } from './Timer'
 
 import './Pomodoro.css'
 
 export function Pomodoro() {
   return (
     <div className="pomodoro">
+      <Cycle />
       <Timer />
-      <Doing task="studying" />
+      <Doing />
     </div>
   )
 }
